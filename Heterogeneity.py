8249c5e3c6f672f831ecd7a037896a0fdcc02b94
Brian Lunder <lunderb@quattro.cs.washington.edu>
2017-03-04 14:43:50 -0800

    changes to spectral and Hetero

--- Heterogeneity.py
+++ Heterogeneity.py
@@ -2,37 +2,59 @@ import pandas as pd
 import numpy as np
 import k_means
 import sys
+import spectral
 from matplotlib import pyplot as plt
 
-def SSD(train, cluster_center, cluster_idk):
+def SSD(train, cluster_center, cluster_idx):
     n = train.shape[0]
     ss = np.zeros((cluster_center.shape[0], ))
     for i in range(n):
-        cluster = cluster_idk[i]
+        cluster = cluster_idx[i]
         diff = train[i, :] - cluster_center[cluster]
         ss[cluster] += np.dot(diff.T, diff)
     return np.sum(ss)
 
 def main():
-    train = pd.read_pickle("tfidf_medium.pkl")
-    heterogeneity = []
-    ks = range(1, 20)
+    train = pd.read_pickle("tfidf_medium_large.pkl")
+    heterogeneity_k_means = []
+    heterogeneity_spectral = []
+    ks = range(1,21)
+    spectral_laplacian = spectral.setup(train)
     for k in ks:
-        bestSSD = sys.maxint
+        print "k: " + str(k)
+        bestSSD_k_means = sys.maxint
+        bestSSD_spectral = sys.maxint
+        spectral_eigen = spectral.computeEigen(spectral_laplacian, k)
         # do clustering 3 times for each k
-        for i in range(5):
-            cluster_center, cluster_idk = k_means.cluster(train.values, k)
-            ssd = SSD(train.values, cluster_center, cluster_idk)
-            if ssd < bestSSD:
-                bestSSD = ssd
+        for i in range(3):
+            print "i: " + str(i)
+            print "k_means"
+            cluster_center_k_means, cluster_idx_k_means = k_means.cluster(train.values, k)
+            ssd_k_means = SSD(train.values, cluster_center_k_means, cluster_idx_k_means)
+            if ssd_k_means < bestSSD_k_means:
+                bestSSD_k_means = ssd_k_means
+            print "Spectral"
+            cluster_center_spectral, cluster_idx_spectral, eigen_vectors = spectral.cluster(spectral_eigen, k)
+            ssd_spectral = SSD(eigen_vectors, cluster_center_spectral, cluster_idx_spectral)
+            if ssd_spectral < bestSSD_spectral:
+                bestSSD_spectral = ssd_spectral
         # append best ssd
-        heterogeneity.append(ssd)
-    plt.plot(ks, heterogeneity, marker=".")
+        heterogeneity_k_means.append(bestSSD_k_means)
+        heterogeneity_spectral.append(bestSSD_spectral)
+    plt.figure(1)
+    plt.plot(ks, heterogeneity_k_means, marker=".")
     plt.ylabel("Heterogeneity")
     plt.xlabel("k")
-    plt.title("k vs Heterogeneity")
+    plt.title("k vs Heterogeneity for k means")
     plt.xticks(np.arange(0, max(ks), 2.0))
-    plt.savefig("heterogeneity.png")
+    plt.savefig("heterogeneity_k_means_ml.png")
+    plt.figure(2)
+    plt.plot(ks, heterogeneity_spectral, marker=".")
+    plt.ylabel("Heterogeneity")
+    plt.xlabel("k")
+    plt.title("k vs Heterogeneity for spectral")
+    plt.xticks(np.arange(0, max(ks), 2.0))
+    plt.savefig("heterogeneity_spectral_ml.png")
 
 if __name__ == "__main__":
     main()
--- spectral.py
+++ spectral.py
@@ -15,12 +15,11 @@ def computeAffinity(train):
     affinity = np.zeros((dim[0], dim[0]))
     sigma = 10
     for i in range(dim[0]):
-        for j in range(dim[0]):
-            # if i == j, skip, don't compare to self
-            if i == j:
-                affinity[i, j] = 0
-                continue
-            affinity[i, j] = np.e**(-1 * distance(train[i, :], train[j,:]) / (2 * (sigma**2)))
+        for j in range(i):
+            aff_calc = np.e**(-1 * distance(train[i, :], train[j,:]) / (2 * (sigma**2)))
+            affinity[i, j] = aff_calc
+            affinity[j, i] = aff_calc
+            
     return affinity
 
 
@@ -43,21 +42,26 @@ def computeEigen(laplacian, k):
     result = U[:,:k]
     return result
 
-def main():
-    global group_num
-    k = int(sys.argv[1])
-    #p = Parser.Parser("tfidf_medium.txt")
-    train = pd.read_pickle("tfidf_medium.pkl")
+def cluster(U, k):
+    # run k_means
+    k_means_result = k_means.cluster(U, k)
+    return (k_means_result[0], k_means_result[1], U)
+
+def setup(train):
     # affinity matrix, is numpy array
     affinity = computeAffinity(train.values)
     # degree matrix, is numpy array
     degree = computeDegree(affinity)
     # laplcian matrix, is numpy array
-    laplacian = computeLaplacian(affinity, degree)
-    # compute eigen vectors
-    U = computeEigen(laplacian, k)
-    # run k_means
-    cluster_center, cluster_idx = k_means.cluster(U, k)
+    return computeLaplacian(affinity, degree)
+
+
+def main():
+    global group_num
+    k = int(sys.argv[1])
+    #p = Parser.Parser("tfidf_medium.txt")
+    train = pd.read_pickle("tfidf_medium.pkl")
+    cluster_center, cluster_idx = cluster(train, k)
     # display the data:
     articles = train.index.values
     groupings = {}
