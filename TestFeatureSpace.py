027a1cd5e481108d193c70f98bbb4a389dfcccee
Daisy <daisy1xu4@gmail.com>
2017-03-06 16:07:04 -0800

    created python file for finding best reduced feature space

---
+++ TestFeatureSpace.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import PCA
+from matplotlib import pyplot as plt
+
+def main():
+    # get dataset
+    data = pd.read_pickle("tfidf_medium.pkl")
+    data_np = data.values
+
+    # get mean
+    mean = PCA.calc_mean(data_np)
+
+    # get error for data space
+    error = []
+    featureSpace = []
+    reconstructError = 0.0
+    k = 1
+    # find smallest feature space to reduce data set
+    while reconstructError > .10:
+        featureSpace.append(k)
+        newSpace = PCA.reduce(data_np, k)
+        reconstructError = PCA.reconstruction_error(newSpace, data_np, mean, k)
+        error.append(reconstructError)
+        k += 1
+    print "Smallest feature space size: " + str(k - 1)
+    plt.plot(featureSpace, error, marker=".")
+    plt.ylabel("Reconstruction Error")
+    plt.xlabel("Size of Reduced Feature Space")
+    plt.title("Size of Reduced Feature Space vs Reconstruction Error")
+    plt.savefig("Error for PCA")
+
+if __name__ == "__main__":
+    main()
